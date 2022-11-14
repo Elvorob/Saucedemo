61c835e3769787b1ca1a54f3cd449cdfe909ae4a
KatyaMy <20FamGam20@gmail.com>
2022-11-14 15:48:09 -0500

    fixed TC 004.02.01 TC 004.04.01 TC 005.01.01 TC 006.02.03 **replaced driver.  by d.

--- test_TC_005_01_01.py
+++ test_TC_005_01_01.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.by import By
 
 
 @pytest.mark.xfail(rises="NotReset")
-def test_reset_app(driver, correct_login):
-    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-    driver.find_element(By.ID, "")
-    driver.find_element(By.ID, "react-burger-menu-btn").click()
+def test_reset_app(d, correct_login):
+    d.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+    d.find_element(By.ID, "")
+    d.find_element(By.ID, "react-burger-menu-btn").click()
     time.sleep(2)
-    driver.find_element(By.ID, "reset_sidebar_link").click()
-    assert driver.find_element(By.ID, "remove-sauce-labs-backpack").text == "ADD TO CART", "NOT RESET"
+    d.find_element(By.ID, "reset_sidebar_link").click()
+    assert d.find_element(By.ID, "remove-sauce-labs-backpack").text == "ADD TO CART", "NOT RESET"
