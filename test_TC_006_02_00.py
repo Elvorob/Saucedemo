6dc026a2728a662502bb409ef282315ad7c2bf83
KatyaMy <20FamGam20@gmail.com>
2022-11-14 18:02:51 -0500

    Added New Test_TC_ 006.02.00

---
+++ test_TC_006_02_00.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+def test_presence_copywriter(d, correct_login):
+    d.find_element(By.CLASS_NAME, "app_logo").click()
+    assert d.title == "Swag Labs", "NOT FOUNDED"
+
+    a = d.find_element(By.CLASS_NAME, "footer_copy")
+    assert a.text == "© 2022 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy", "copywriter NOT FOUND"
+
+    img = d.find_element(By.XPATH, '//body/div[@id="root"]/div[@id="page_wrapper"]/footer[1]/img[1]')
+    print(f"IMG Present_{img.is_displayed()}")
