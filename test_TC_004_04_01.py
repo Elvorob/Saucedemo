60fcb6bc6fb501059bb1528affc1edca8d62d512
Elena Vorobyeva <elvorob@gmail.com>
2022-11-14 17:24:33 -0500

    Замена driver на d в тестах KatyaMy

--- test_TC_004_04_01.py
+++ test_TC_004_04_01.py
@@ -5,12 +5,12 @@ from pages.cart_page import *
 
 
 @pytest.mark.xfail(rises="NotWorking")
-def test_change_qty(driver, correct_login):
-    assert driver.title == "Swag Labs", "____YOU NOT ENTER______"
-    driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
+def test_change_qty(d, correct_login):
+    assert d.title == "Swag Labs", "____YOU NOT ENTER______"
+    d.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
     time.sleep(3)
-    cart = CartPage(driver, link)
+    cart = CartPage(d, link)
     cart.click_icon_cart()
-    driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]").click()
+    d.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]").click()
 
     time.sleep(3)
