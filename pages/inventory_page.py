a920b52edcd4bd002f627d3184f910dba673a3de
Anna Dolmatova <annadolmatova01@gmail.com>
2022-11-15 12:18:12 -0500

    added test_TC_002_07_01.py, added locator, added inventory_page methods

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -18,3 +18,14 @@ class InventoryPage(BasePage):
     def change_quantity_in_cart(self):
         self.add_to_cart_backpack_inventory()
 
+    def add_to_cart_backpack(self):
+        self.d.find_element(*InventoryPageLocators.BACKPACK_ADD_BTN).click()
+
+    def item_added_to_cart(self):
+        assert self.element_is_present(*InventoryPageLocators.CART_BADGE)
+        assert self.element_is_present(*InventoryPageLocators.BACKPACK_REMOVE_BTN)
+
+    def no_item_in_cart(self):
+        assert self.element_is_NOT_present(*InventoryPageLocators.CART_BADGE)
+        assert self.element_is_NOT_present(*InventoryPageLocators.BACKPACK_REMOVE_BTN)
+
