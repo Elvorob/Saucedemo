02018fa514b3e1e06f8520a9b580ab45819ead38
Elena Vorobyeva <elvorob@gmail.com>
2022-11-15 17:24:44 -0500

    test TC_001.00.07 was added

--- test_TC_003_03_01.py
+++ test_TC_003_03_01.py
@@ -10,21 +10,27 @@ def test_compare_item_names(d, correct_login):
 
 
 def test_compare_item_desc(d, correct_login):
-    get_name_list = d.find_element(*InventoryPageLocators.BIKELIGHT_DESCRIPTION).text
+    get_desc_list = d.find_element(*InventoryPageLocators.BIKELIGHT_DESCRIPTION).text
     d.find_element(*InventoryPageLocators.BIKELIGHT_LABEL).click()
-    get_name_card = d.find_element(*InventoryItemPageLocator.INVENTORY_ITMEM_DESCRIPTION).text
-    assert get_name_list == get_name_card, "Item description !ARE NOT! the same"
+    get_desc_card = d.find_element(
+        *InventoryItemPageLocator.INVENTORY_ITMEM_DESCRIPTION
+    ).text
+    assert get_desc_list == get_desc_card, "Item description !ARE NOT! the same"
 
 
 def test_compare_item_price(d, correct_login):
-    get_name_list = d.find_element(*InventoryPageLocators.BIKELIGHT_PRICE).text
+    get_price_list = d.find_element(*InventoryPageLocators.BIKELIGHT_PRICE).text
     d.find_element(*InventoryPageLocators.BIKELIGHT_LABEL).click()
-    get_name_card = d.find_element(*InventoryItemPageLocator.INVENTORY_ITMEM_PRICE).text
-    assert get_name_list == get_name_card, "Item Price !ARE NOT! the same"
+    get_price_card = d.find_element(*InventoryItemPageLocator.INVENTORY_ITMEM_PRICE).text
+    assert get_price_list == get_price_card, "Item Price !ARE NOT! the same"
 
 
 def test_compare_item_img(d, correct_login):
-    get_name_list = d.find_element(*InventoryPageLocators.BIKELIGHT_IMG).get_attribute("src")
+    get_img_list = d.find_element(*InventoryPageLocators.BIKELIGHT_IMG).get_attribute(
+        "src"
+    )
     d.find_element(*InventoryPageLocators.BIKELIGHT_LABEL).click()
-    get_name_card = d.find_element(*InventoryItemPageLocator.INVENTORY_ITMEM_IMG).get_attribute("src")
-    assert get_name_list == get_name_card, "Item image !ARE NOT! the same"
+    get_img_card = d.find_element(
+        *InventoryItemPageLocator.INVENTORY_ITMEM_IMG
+    ).get_attribute("src")
+    assert get_img_list == get_img_card, "Item image !ARE NOT! the same"
