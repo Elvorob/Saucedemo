0ad9cfe16bd000a4176d68567f31d3793f505bcb
Eliser14 <elinaa@Elinas-Air.attlocal.net>
2022-11-25 23:02:38 -0600

    Added file pytest.ini. Added new file conftest.py. Added 3 tests

---
+++ conftest_old.py
@@ -0,0 +1,86 @@
+# import pytest
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from .pages.locators import *
+# from selenium.webdriver.firefox.service import Service as FirefoxService
+# from webdriver_manager.firefox import GeckoDriverManager
+# from webdriver_manager.core.utils import ChromeType
+#
+#
+# @pytest.fixture(scope="function")
+# def correct_login(d):
+#     d.get("https://www.saucedemo.com/")
+#     d.find_element(*LoginPageLocators.USERNAME_INPUT).send_keys(
+#         LoginPageLocators.USER_NAME
+#     )
+#     d.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(
+#         LoginPageLocators.PASSWORD
+#     )
+#     d.find_element(*LoginPageLocators.LOGIN_BTN).click()
+#     assert (
+#         d.current_url == "https://www.saucedemo.com/inventory.html"
+#     ), "____YOU NOT ENTER______"
+#
+#
+# @pytest.fixture(scope="class")
+# def d(browser):
+#     if browser == "firefox":
+#         o = webdriver.FirefoxOptions()
+#         o.headless = True
+#         driver = webdriver.Firefox(
+#             service=FirefoxService(GeckoDriverManager().install()), options=o
+#         )
+#     else:
+#         o = webdriver.ChromeOptions()
+#         o.headless = True
+#         driver = webdriver.Chrome(
+#             service=ChromeService(ChromeDriverManager().install()), options=o
+#         )
+#     return driver
+#
+#
+# # ------- 2 функции определяют параметр, котрый принимает pytest (browser)
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--browser",
+#         default="chrome",
+#         help="define browser: chrome or firefox, --firefox",
+#     )
+#
+#
+# # ------- browser сами назвали так параметр
+# # ------- default - что будет запускаться по умолчанию
+# @pytest.fixture(scope="class")
+# def browser(request):
+#     return request.config.getoption("--browser")
+#
+#
+# ######################################################################
+# @pytest.fixture(scope="class", autouse=True)
+# def g(d):
+#     print("\n***** start fixture = setup *****\n")
+#     d.get("https://www.saucedemo.com/")
+#     yield d
+#     d.quit()
+#     print("\n***** end fixture = teardown *****\n")
+#
+#
+# # --------- указывать другое имя для отчетов.
+# # --------- По умолчанию название report.html
+# # def pytest_html_report_title(report):
+# #     report.title = "Blablabla"
+#
+# '''
+# -----------------------------------------------------
+# ------------ Sign in with valid username ------------
+# -----------------------------------------------------
+# '''
+# @pytest.fixture(params=['standard_user', "problem_user", "performance_glitch_user"],
+#                 scope='function')
+# def login_from_list(d, request):
+#     d.get('https://www.saucedemo.com/')
+#     d.find_element(*LoginPageLocators.USERNAME_INPUT).send_keys(request.param)
+#     d.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys('secret_sauce')
+#     d.find_element(*LoginPageLocators.LOGIN_BTN).click()
+#
