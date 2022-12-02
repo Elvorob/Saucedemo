0c37b63294f0690ece93b445763ac739cfcc54cb
Ekaterina <luchik.eg@gmail.com>
2022-12-01 23:06:43 -0600

    Added the test for TC 002.02.01

--- tests/test_TC_002_02_01.py
+++ tests/test_TC_002_02_01.py
@@ -7,9 +7,9 @@ def test_product_cart_new_page(d, correct_login):
     d.maximize_window()
     achains = ActionChains(d)
     backpack = d.find_element(*InventoryPageLocators.BACKPACK_LINK).click()
-    # right click
     achains.context_click(backpack).perform()
     d.implicitly_wait(1)
-    # choose in context menu "open in new window"
-    achains.context_click(backpack).send_keys(Keys.ARROW_DOWN).send_keys(Keys.RETURN).perform()
+    achains.context_click(backpack).send_keys(Keys.ARROW_DOWN).send_keys(
+        Keys.RETURN
+    ).perform()
     assert d.current_url == "https://www.saucedemo.com/inventory-item.html?id=4"
