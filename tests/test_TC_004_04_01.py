3da8594bbf2f1714e170101058867368a5be88f0
KatyaMy <20FamGam20@gmail.com>
2022-12-08 18:31:32 -0500

    merged 2 tests into one

--- tests/test_TC_004_04_01.py
+++ tests/test_TC_004_04_01.py
@@ -1,10 +1,24 @@
+import time
+
 import pytest
 from ..pages.cart_page import *
 
 
-@pytest.mark.xfail(rises="NotWorking")
-def test_change_qty(d, correct_login):
+@pytest.mark.xfail(rises="NotWorking_BUG,not implemented")
+def test_change_qty_plus(d, correct_login):
     d.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
+    cart = CartPage(d, link)
+    cart.click_icon_cart()
+    d.find_element(
+        By.XPATH,
+        "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]",
+    ).click()
+
+
+@pytest.mark.xfail(rises="NotWorking_BUG,not implemented")
+def test_change_dty_minus(d, correct_login):
+    d.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
+    time.sleep(2)
     cart = CartPage(d, link)
     cart.click_icon_cart()
     d.find_element(
